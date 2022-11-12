6043510a83eeca81e495e0aa370af69229409b3c
Linxinyang2008 <104971677+Linxinyang2008@users.noreply.github.com>
2022-11-12 21:07:13 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,13 +1,16 @@
+#define Ex_io
+
 #include"exint.hpp"
 #include<iostream>
 using namespace std;
 exint<int> ex1, ex2;
 
-#define Ex_io
-
 int main(void){
 	string str = "100123";
 	exread_string(ex1);exread_string(ex2);
-	cout << ex1 + ex2 << endl;// << ex1 - ex2 << endl << ex1 * ex2 << endl << ex1 / ex2 << endl << ex1 % ex2 << endl;
+	cout << ex1 + ex2 << endl << ex1 - ex2 << endl << ex1 * ex2 << endl << ex1 / ex2 << endl << ex1 % ex2 << endl;
+	cout << -ex1 << ' ' ;
+	
+	cout << "ValueNameToString(ex1)" << endl;
 	return 0;
 }
